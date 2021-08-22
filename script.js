8e37b8ff12460f02306bbdf0942eefea9bd2bca4
nguyenkimdao1998 <nguyenkimdao1998@gamil.com>
2021-08-22 18:13:22 +0700

    update final ver4

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const menuBar = document.querySelector(".menu-bar")
-menuBar.addEventListener("click",function(){
-	menuBar.classList.toggle("active")
-	document.querySelector(".menu-items").classList.toggle("active")
-})
 
 const toP=document.querySelector(".top")
 window.addEventListener("scroll",function(){
